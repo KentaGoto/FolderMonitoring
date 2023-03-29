4bbaee37b70f9d0da1555b05a27f982949d444b8
KentaGoto <kenta-goto@to-in.co.jp>
2023-03-29 15:36:24 +0900

    Progress bar changed.

--- FolderMonitoring.ps1
+++ FolderMonitoring.ps1
@@ -1,21 +1,22 @@
 $Seconds = 5 # Interval of seconds to monitor.
 $Path = "\\nf-srv\temp\goto\test\t" # Path to monitor.
-$Dots = "" # Progress mark "*".
 
-while ($true) {
-    Clear-Host
-    Write-Host "Monitoring..."
-    Write-Host
-
+$monitoring = $true
+while ($monitoring) {
     $FileCount = (Get-ChildItem -Path $Path | Measure-Object).Count
+
     if ($FileCount -eq 0) {
-        $Dots += "*"
-        Write-Host $Dots
-        Start-Sleep -Seconds $Seconds
+        for ($i = 0; $i -lt $Seconds; $i++) {
+            $progressPercentage = ($i / $Seconds) * 100
+            Write-Progress -Activity "Monitoring..." -Status "Waiting for files" -PercentComplete $progressPercentage
+            Start-Sleep -Seconds 1
+        }
     } else {
-        break
+        $monitoring = $false
     }
 }
+
+Write-Progress -Activity "Monitoring..." -Status "File detected" -Completed
 
 Write-Host ""
 Write-Host "┏━━━━━━━━━━━━━━━━━━━━━┓"
